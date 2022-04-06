2e289a3bdad240b8e76d52093a7310689560773f
baekchan1024 <baekchan1024@gmail.com>
2022-04-06 18:14:31 +0900

    [fix] 불필요한 주석 제거

--- src/main/java/com/backend/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/backend/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -15,7 +15,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.URI;
 import java.util.Optional;
 
@@ -45,11 +44,6 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
             logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
             return;
         }
-
-//        response.setStatus(302);
-//        response.setHeader("Location", "http://localhost:3000");
-//        PrintWriter writer = response.getWriter();
-//        writer.println("hello world");
 
         clearAuthenticationAttributes(request, response);
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
