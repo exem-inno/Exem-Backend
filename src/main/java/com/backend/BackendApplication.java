3adc366b6ba8f2f98f90e4cd7ff80180fcffa5f8
baekchan1024 <baekchan1024@gmail.com>
2022-04-14 16:32:28 +0900

    feature 꼬여서 다시 정리

--- src/main/java/com/backend/BackendApplication.java
+++ src/main/java/com/backend/BackendApplication.java
@@ -1,17 +1,14 @@
 package com.backend;
 
 import com.backend.config.AppProperties;
-import com.backend.repository.ProductRepository;
 import com.backend.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
-@EnableMongoRepositories(basePackageClasses = {ProductRepository.class})
 @EnableConfigurationProperties({AppProperties.class})
 public class BackendApplication {
 
--- src/main/java/com/backend/model/Fields.java
+++
@@ -1,9 +0,0 @@
-package com.backend.model;
-
-import lombok.Data;
-
-@Data
-public class Fields {
-    private String key;
-    private String value;
-}
